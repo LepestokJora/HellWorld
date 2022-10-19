46ed06a97933ed52bc84947d2d941f7905862834
Artem Peshkov <pewkoff@gmail.com>
2022-10-19 22:29:43 +0300

    hi word

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Lepestok Jora on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
